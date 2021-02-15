e0b01f97f7757b83e9fc2714879105777257a7bb
Bjorn de Mul <bjorndemul@gmail.com>
2021-02-15 19:42:53 +0100

    fix: typo

--- mod_bavio_news_home.php
+++ mod_bavio_news_home.php
@@ -5,7 +5,7 @@ require_once dirname(__FILE__) . '/helper.php';
 
 $sub_title = $params->get('sub_title', 'UNKNOWN');
 $title = $params->get('title', 'UNKNOWN');
-$read_more = $params->get('readm_more', 'UNKNOWN');
+$read_more = $params->get('read_more', 'UNKNOWN');
 $url_text = $params->get('url_text', 'UNKNOWN');
 $menu_item = ModBavioNewsHome::getMenuItem($params->get('menu_item', '-1'));
 $articles = ModBavioNewsHome::getArticlesFromCategories($params->get('sub-categories', '0'), $params->get('categories', []), $params->get('limit', 6));
